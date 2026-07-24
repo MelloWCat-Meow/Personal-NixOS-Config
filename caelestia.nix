604376a2198f621e630bc1a7b5d848ea41882c21
MelloWCat <alansuranjana2103@gmail.com>
2026-07-24 18:10:18 +0700

    Add Terminal and Simple Keybind

--- caelestia.nix
+++ caelestia.nix
@@ -23,12 +23,39 @@
       recursive = true;
     };
 
+    home.packages = with pkgs; [ foot ];
+
     # Autostart caelestia shell saat masuk session Hyprland
     wayland.windowManager.hyprland = {
       enable = true;
       configType = "hyprlang";
       settings = {
+        "$mod" = "SUPER";
+        "$terminal" = "foot";
         "exec-once" = [ "caelestia shell -d" ];
+
+        bind = [
+          "$mod, Return, exec, $terminal"
+          "$mod, Q, killactive"
+          "$mod, M, exit"
+          "$mod, V, togglefloating"
+          "$mod, F, fullscreen"
+          "$mod, D, global, caelestia:launcher"
+
+          "$mod, 1, workspace, 1"
+          "$mod, 2, workspace, 2"
+          "$mod, 3, workspace, 3"
+          "$mod, 4, workspace, 4"
+          "$mod SHIFT, 1, movetoworkspace, 1"
+          "$mod SHIFT, 2, movetoworkspace, 2"
+          "$mod SHIFT, 3, movetoworkspace, 3"
+          "$mod SHIFT, 4, movetoworkspace, 4"
+      ];
+
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
       };
     };
   };
